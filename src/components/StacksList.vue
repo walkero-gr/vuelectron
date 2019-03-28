c3e4feb1345f4aebfe8bce629924b234249868be
George Sokianos <george.sokianos@WRLT0258.local>
2019-03-28 16:53:00 +0000

    Created tha stack list, using the Container List component

--- src/components/StacksList.vue
+++ src/components/StacksList.vue
@@ -6,7 +6,7 @@
       <v-flex xs-12>
         <v-app id="stacksList">
             <v-card>
-                <!-- <v-card-title>
+                <v-card-title>
                     <v-spacer></v-spacer>
                     <v-text-field
                     v-model="search"
@@ -15,21 +15,23 @@
                     single-line
                     hide-details
                     ></v-text-field>
-                </v-card-title> -->
+                </v-card-title>
                 <v-data-table
                 :headers="headers"
                 :items="stacks"
                 :search="search"
                 class="elevation-1"
+                item-key="name"
                 >
                 <template v-slot:items="stacks">
-                    <td>{{ stacks.item.name }}</td>
-                    <!-- <td class="text-xs-center">
-                        <v-chip :color="containers.item.statusInfo[0].color" text-color="white" small>{{ containers.item.statusInfo[0].text }}</v-chip>
-                    </td>
-                    <td class="text-xs-left">{{ containers.item.stack }}</td>
-                    <td class="text-xs-left">{{ containers.item.imageName }}</td>
-                    <td class="text-xs-left">{{ containers.item.createdString }}</td> -->
+                    <tr @click="stacks.expanded = !stacks.expanded">
+                        <td>{{ stacks.item.name }}</td>
+                    </tr>
+                </template>
+                <template v-slot:expand="stacks">
+                    <v-card flat>
+                        <ContainersList :containers="stacks.item.containers" />
+                    </v-card>
                 </template>
                 <template v-slot:no-data>
                     <v-alert :value="true" color="error" icon="error">
@@ -46,88 +48,24 @@
 
 
 <script>
-  const got = require('got')
-  var moment = require('moment')
-  var voca = require('voca')
+  import ContainersList from './ContainersList'
 
   export default {
+    components: {
+      ContainersList
+    },
+    props: ['stacks'],
     data () {
         return {
             search: '',
             headers: [
             {
-                text: 'Container',
+                text: 'Stack name',
                 align: 'left',
                 sortable: true,
                 value: 'name'
-            },
-            // { 
-            //     text: 'Status', align: 'center', value: 'name' 
-            // },
-            // { 
-            //     text: 'Stack', align: 'left', value: 'name' 
-            // },
-            // { 
-            //     text: 'Image', align: 'left', value: 'name' 
-            // },
-            // { 
-            //     text: 'Created', align: 'left', value: 'name' 
-            // }
-            ],
-            stacks: []
-        }
-    },
-    created () {
-        (async () => {
-            try {
-                const response = await got.get('unix:/var/run/docker.sock:/v1.39/containers/json?all=true',
-                {
-                    json: true
-                })
-                let containersData = response.body
-                let stacksData = []
-                containersData.forEach(element => {
-                    element.containerName = voca.splice(element.Names[0], 0, 1)
-                    element.stack = (element.Labels['com.docker.compose.project']) ? element.Labels['com.docker.compose.project'] : '-'
-                    element.statusInfo = this.getStatusInfo(element.State)
-                    element.createdString = moment(element.Created*1000).format('YYYY-MM-DD')
-                    element.imageName = element.Image
-
-                    if(!stacksData[element.stack]) {
-                        stacksData[element.stack] = {
-                            name: element.stack,
-                            containers: []
-                        }
-                    }
-                    stacksData[element.stack].containers.push(element)
-                });
-
-                let stacks = Object.keys(stacksData).map((key) => {
-                    return stacksData[key]
-                })
-                
-                console.log(stacks)
-                this.stacks = stacks
-            } catch (error) {
-                console.log(error.response.body)
             }
-        })()
-    },
-    methods: {
-        getStatusInfo(state) {
-            // primary, secondary, success, info, warning, error
-            if (state == 'running') {
-                return [{
-                    text: 'running',
-                    color: 'success'
-                }]
-            }
-            if (state == 'exited') {
-                return [{
-                    text: 'stopped',
-                    color: 'error'
-                }]
-            }
+            ]
         }
     }
   }
--- src/views/Stacks.vue
+++ src/views/Stacks.vue
@@ -7,17 +7,80 @@
       text-xs-center
       wrap
     >
-      <StacksList />
+      <StacksList :stacks="stacks" />
     </v-layout>
   </v-container>
 </template>
 
 <script>
+  const got = require('got')
+  var moment = require('moment')
+  var voca = require('voca')
+
   import StacksList from '../components/StacksList'
 
   export default {
     components: {
       StacksList
+    },
+    data () {
+        return {
+            stacks: []
+        }
+    },
+    created () {
+        (async () => {
+            try {
+                const response = await got.get('unix:/var/run/docker.sock:/v1.39/containers/json?all=true',
+                {
+                    json: true
+                })
+                let containersData = response.body
+                let stacksData = []
+                containersData.forEach(element => {
+                    element.containerName = voca.splice(element.Names[0], 0, 1)
+                    element.stack = (element.Labels['com.docker.compose.project']) ? element.Labels['com.docker.compose.project'] : '-'
+                    element.statusInfo = this.getStatusInfo(element.State)
+                    element.createdString = moment(element.Created*1000).format('YYYY-MM-DD')
+                    element.imageName = element.Image
+                    if(element.stack != '-') {
+                        if(!stacksData[element.stack]) {
+                            stacksData[element.stack] = {
+                                name: element.stack,
+                                containers: []
+                            }
+                        }
+                        stacksData[element.stack].containers.push(element)
+                    }
+                });
+
+                let stacks = Object.keys(stacksData).map((key) => {
+                    return stacksData[key]
+                })
+                
+                // console.log(stacks)
+                this.stacks = stacks
+            } catch (error) {
+                console.log(error.response.body)
+            }
+        })()
+    },
+    methods: {
+        getStatusInfo(state) {
+            // primary, secondary, success, info, warning, error
+            if (state == 'running') {
+                return [{
+                    text: 'running',
+                    color: 'success'
+                }]
+            }
+            if (state == 'exited') {
+                return [{
+                    text: 'stopped',
+                    color: 'error'
+                }]
+            }
+        }
     }
   }
 </script>
