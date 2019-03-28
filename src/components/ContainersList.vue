a8a606c693fd595c912b94301e8ace68bcf52ea8
George Sokianos <george.sokianos@WRLT0258.local>
2019-03-28 16:32:19 +0000

    Moved the containers list data logic to the parent component. Now the listing is more individual and can be reused

--- src/components/ContainersList.vue
+++ src/components/ContainersList.vue
@@ -46,80 +46,35 @@
 
 
 <script>
-  const got = require('got')
-  var moment = require('moment')
-  var voca = require('voca')
 
-  export default {
-    data () {
-        return {
-            search: '',
-            headers: [
-            {
-                text: 'Container',
-                align: 'left',
-                sortable: true,
-                value: 'containerName'
-            },
-            { 
-                text: 'Status', align: 'center', value: 'State' 
-            },
-            { 
-                text: 'Stack', align: 'left', value: 'stack' 
-            },
-            { 
-                text: 'Image', align: 'left', value: 'imageName' 
-            },
-            { 
-                text: 'Created', align: 'left', value: 'createdString' 
-            }
-            ],
-            containers: []
-        }
-    },
-    created () {
-        (async () => {
-            try {
-                const response = await got.get('unix:/var/run/docker.sock:/v1.39/containers/json?all=true',
+    export default {
+        props: ['containers'],
+        data () {
+            return {
+                search: '',
+                headers: [
                 {
-                    json: true
-                })
-                let containersData = response.body
-                containersData.forEach(element => {
-                    element.containerName = voca.splice(element.Names[0], 0, 1)
-                    element.stack = (element.Labels['com.docker.compose.project']) ? element.Labels['com.docker.compose.project'] : '-'
-                    element.statusInfo = this.getStatusInfo(element.State)
-                    element.createdString = moment(element.Created*1000).format('YYYY-MM-DD')
-                    element.imageName = element.Image
-                    if(voca.indexOf(element.Image, 'sha256:') >= 0) {
-                        element.imageName = voca.splice(element.Image, 0, 7)
-                        element.imageName = voca.splice(element.imageName, 12)
-                    }
-                });
-                this.containers = containersData
-            } catch (error) {
-                console.log(error.response.body)
-            }
-        })()
-    },
-    methods: {
-        getStatusInfo(state) {
-            // primary, secondary, success, info, warning, error
-            if (state == 'running') {
-                return [{
-                    text: 'running',
-                    color: 'success'
-                }]
-            }
-            if (state == 'exited') {
-                return [{
-                    text: 'stopped',
-                    color: 'error'
-                }]
+                    text: 'Container',
+                    align: 'left',
+                    sortable: true,
+                    value: 'containerName'
+                },
+                { 
+                    text: 'Status', align: 'center', value: 'State' 
+                },
+                { 
+                    text: 'Stack', align: 'left', value: 'stack' 
+                },
+                { 
+                    text: 'Image', align: 'left', value: 'imageName' 
+                },
+                { 
+                    text: 'Created', align: 'left', value: 'createdString' 
+                }
+                ]
             }
         }
     }
-  }
 </script>
 
 <style>
--- src/views/Containers.vue
+++ src/views/Containers.vue
@@ -7,17 +7,68 @@
       text-xs-center
       wrap
     >
-      <ContainersList />
+      <ContainersList :containers="containers" />
     </v-layout>
   </v-container>
 </template>
 
 <script>
+  const got = require('got')
+  var moment = require('moment')
+  var voca = require('voca')
+
   import ContainersList from '../components/ContainersList'
 
   export default {
     components: {
       ContainersList
+    },
+    data () {
+        return {
+            containers: []
+        }
+    },
+    created () {
+        (async () => {
+            try {
+                const response = await got.get('unix:/var/run/docker.sock:/v1.39/containers/json?all=true',
+                {
+                    json: true
+                })
+                let containersData = response.body
+                containersData.forEach(element => {
+                    element.containerName = voca.splice(element.Names[0], 0, 1)
+                    element.stack = (element.Labels['com.docker.compose.project']) ? element.Labels['com.docker.compose.project'] : '-'
+                    element.statusInfo = this.getStatusInfo(element.State)
+                    element.createdString = moment(element.Created*1000).format('YYYY-MM-DD')
+                    element.imageName = element.Image
+                    if(voca.indexOf(element.Image, 'sha256:') >= 0) {
+                        element.imageName = voca.splice(element.Image, 0, 7)
+                        element.imageName = voca.splice(element.imageName, 12)
+                    }
+                });
+                this.containers = containersData
+            } catch (error) {
+                console.log(error.response.body)
+            }
+        })()
+    },
+    methods: {
+        getStatusInfo(state) {
+            // primary, secondary, success, info, warning, error
+            if (state == 'running') {
+                return [{
+                    text: 'running',
+                    color: 'success'
+                }]
+            }
+            if (state == 'exited') {
+                return [{
+                    text: 'stopped',
+                    color: 'error'
+                }]
+            }
+        }
     }
   }
 </script>
